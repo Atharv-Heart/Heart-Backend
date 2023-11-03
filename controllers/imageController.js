54fffb77bed533191c3cf8d61dfd404fadcca3ba
Ishika Jain <ishika2413@gmail.com>
2023-11-04 02:41:49 +0530

    DG

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,5 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 const Image = require("../models/Image");
+const { v4: uuidv4 } = require('uuid'); 
+
 
 const uploadImage = async (req, res) => {
   try {
@@ -31,14 +33,23 @@ const getImages = async (req, res) => {
       .expression(`folder:${folder}`)
       .execute();
 
-    const urls = result.resources.map((resource) => resource.url);
+    const images = result.resources.map((resource) => {
+      return {
+        _id: uuidv4(), 
+        userId: "65414829f26c7b12e7b4f708", 
+        text: "text i want here", 
+        imageUrl: resource.url,
+        __v: 0
+      };
+    });
 
-    res.status(200).json(urls);
+    res.status(200).json(images);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error fetching images" });
   }
 };
+
 
 module.exports = {
   uploadImage,
